3bc73b5c66b17bfeed2d0500d46f8c0f7230e895
yunshou <yun.shou@msa.hinet.net>
2017-09-03 21:37:15 +0800

    1.0.11-4

---
+++ web/test.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+            $json = file_get_contents('https://spreadsheets.google.com/feeds/list/1tQCaj3LUVwH0tBuPrfBY2dOJuF-qzpYEdOqGdNvJRLc/od6/public/values?alt=json');
+            $data = json_decode($json, true);
+            $result = array();
+           
+            $message['text'] = '彰化';
+           
+            foreach ($data['feed']['entry'] as $item) {
+                $keywords = explode(',', $item['gsx$keyword']['$t']);
+                foreach ($keywords as $keyword) {
+                    if (mb_strpos($message['text'], $keyword) !== false) {
+                        $candidate = array(
+                            'thumbnailImageUrl' => $item['gsx$photourl']['$t'],
+                            'title' => $item['gsx$title']['$t'],
+                            'text' => $item['gsx$title']['$t'],
+                            'actions' => array(
+                                array(
+                                'type' => 'uri',
+                                'label' => '查看詳情',
+                                'uri' => $item['gsx$url']['$t'],
+                                ),
+                            ),
+                        );
+                        array_push($result, $candidate);
+                    }
+                }
+            }
+
+           print_r($result);
+?>
