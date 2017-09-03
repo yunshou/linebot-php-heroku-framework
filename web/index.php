6cd4d04954491a8b6f85983bb23d907c55c550ca
yunshou <yun.shou@msa.hinet.net>
2017-09-03 20:28:35 +0800

    1.0.10 加入 google 試算表

--- web/index.php
+++ web/index.php
@@ -18,8 +18,6 @@
 
 require_once('./LINEBotTiny.php');
 
-
-
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
 
@@ -28,81 +26,147 @@ foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
             $message = $event['message'];
+
+            //https://docs.google.com/spreadsheets/d/e/2PACX-1vQo09zfHQAye7n2vW--8EpR5DxmuCawBiCuDM9p7PzhOvuKQtiAeafNQLBX64A5s4PFEvikZpKdSz3a/pubhtml
+            $json = file_get_contents('https://spreadsheets.google.com/feeds/list/2PACX-1vQo09zfHQAye7n2vW--8EpR5DxmuCawBiCuDM9p7PzhOvuKQtiAeafNQLBX64A5s4PFEvikZpKdSz3a/od6/public/values?alt=json');
+            $data = json_decode($json, true);
+            $result = array();
+            $isKeyWord ='0';
+
+            foreach ($data['feed']['entry'] as $item) {
+                $keywords = explode(',', $item['gsx$keyword']['$t']);
+                foreach ($keywords as $keyword) {
+                    if (mb_strpos($message['text'], $keyword) !== false) {
+                        $candidate = array(
+                            'thumbnailImageUrl' => $item['gsx$photourl']['$t'],
+                            'title' => $item['gsx$title']['$t'],
+                            'text' => $item['gsx$title']['$t'],
+                            'actions' => array(
+                                array(
+                                'type' => 'uri',
+                                'label' => '查看詳情',
+                                'uri' => $item['gsx$url']['$t'],
+                                ),
+                            ),
+                        );
+                    array_push($result, $candidate);
+                     $isKeyWord = '1';
+                    }else{
+                      $isKeyWord = '0';
+
+                    }
+                }
+            }
+
             switch ($message['type']) {
                 case 'text':
-                	$m_message = $message['text'];
-                	if($m_message!="" && preg_match('/\肉肉/i',$m_message))
-                	{
-                		$client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(
-                                'type' => 'text',
-                                'text' => '我是好吃的肉肉！'
-                                 //'text' => $m_message
+                    if($isKeyWord == '1'){
+                        $client->replyMessage(array(
+                            'replyToken' => $event['replyToken'],
+                            'messages' => array(
+                                array(
+                                    'type' => 'text',
+                                    'text' => $message['text'].'讓我想想喔…',
+                                ),
+                                array(
+                                    'type' => 'template',
+                                    'altText' => '為您推薦下列美食：',
+                                    'template' => array(
+                                        'type' => 'carousel',
+                                        'columns' => $result,
+                                    ),
+                                ),
+                                array(
+                                    'type' => 'text',
+                                    'text' => '這些都超好吃，真心不騙！',
+                                ),
+                                array(
+                                    'type' => 'sticker',
+                                    'packageId' => '1',
+                                    'stickerId' => '2',
+                                ),
+                            ),
+                        ));
+                    }else{
+                        $m_message = $message['text'];
+                        if($m_message!="" && preg_match('/\肉肉/i',$m_message))
+                        {
+                            $client->replyMessage(array(
+                            'replyToken' => $event['replyToken'],
+                            'messages' => array(
+                                array(
+                                    'type' => 'text',
+                                    'text' => '我是好吃的肉肉！'
+                                    //'text' => $m_message
+                                )
                             )
-                        )
-                    	));
-                	}
-                	if($m_message!="" && preg_match('/\雲手/i',$m_message))
-                	{
-                		$client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(
-                                'type' => 'text',
-                                'text' => '我的雲手好帥歐！'
-                                 //'text' => $m_message
+                            ));
+                        }
+                        if($m_message!="" && preg_match('/\雲手/i',$m_message))
+                        {
+                            $client->replyMessage(array(
+                            'replyToken' => $event['replyToken'],
+                            'messages' => array(
+                                array(
+                                    'type' => 'text',
+                                    'text' => '我的雲手好帥歐！'
+                                    //'text' => $m_message
+                                )
                             )
-                        )
-                    	));
-                	}
-                	if($m_message!="" && preg_match('/\阿軒/i',$m_message))
-                	{
-                		$client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(
-                                'type' => 'text',
-                                'text' => '阿軒！要一起玩肉肉嗎？((害羞))'
-                                 //'text' => $m_message
+                            ));
+                        }
+                        if($m_message!="" && preg_match('/\阿軒/i',$m_message))
+                        {
+                            $client->replyMessage(array(
+                            'replyToken' => $event['replyToken'],
+                            'messages' => array(
+                                array(
+                                    'type' => 'text',
+                                    'text' => '阿軒！要一起玩肉肉嗎？((害羞))'
+                                    //'text' => $m_message
+                                )
                             )
-                        )
-                    	));
-                	}
-                	if($m_message!="" && preg_match('/\幹/i',$m_message))
-                	{
-                		$client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(
-                                'type' => 'text',
-                                'text' => '不要罵髒話啦～人家會害怕！！'
-                                 
+                            ));
+                        }
+                        if($m_message!="" && preg_match('/\幹/i',$m_message))
+                        {
+                            $client->replyMessage(array(
+                            'replyToken' => $event['replyToken'],
+                            'messages' => array(
+                                array(
+                                    'type' => 'text',
+                                    'text' => '不要罵髒話啦～人家會害怕！！'
+                                    
+                                )
                             )
-                        )
-                    	));
-                	}
-                	if($m_message!="" && preg_match('/\已讀不回/i',$m_message))
-                	{
-                		$client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(            
-                                'type'=> 'image',
-                                'originalContentUrl' => 'https://i.imgur.com/xjTET8v.jpg',
-                                'previewImageUrl'=> 'https://i.imgur.com/xjTET8v.jpg'                                       
+                            ));
+                        }
+                        if($m_message!="" && preg_match('/\已讀不回/i',$m_message))
+                        {
+                            $client->replyMessage(array(
+                            'replyToken' => $event['replyToken'],
+                            'messages' => array(
+                                array(            
+                                    'type'=> 'image',
+                                    'originalContentUrl' => 'https://i.imgur.com/xjTET8v.jpg',
+                                    'previewImageUrl'=> 'https://i.imgur.com/xjTET8v.jpg'                                       
+                                )
                             )
-                        )
-                    	));
-                	}
+                            ));
+                        }
+                    }
 
-                break;
-                
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
             }
             break;
+
+
         default:
             error_log("Unsupporeted event type: " . $event['type']);
             break;
     }
 };
+
