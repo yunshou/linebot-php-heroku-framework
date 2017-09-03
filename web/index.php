3e1d3c15f32783eae76a9a1b00ff93c72a332af1
yunshou <yun.shou@msa.hinet.net>
2017-09-03 21:03:35 +0800

    1.0.11-3

--- web/index.php
+++ web/index.php
@@ -27,70 +27,67 @@ foreach ($client->parseEvents() as $event) {
         case 'message':
             $message = $event['message'];
 
-            //https://docs.google.com/spreadsheets/d/e/2PACX-1vQo09zfHQAye7n2vW--8EpR5DxmuCawBiCuDM9p7PzhOvuKQtiAeafNQLBX64A5s4PFEvikZpKdSz3a/pubhtml
             
-            //$json = file_get_contents('https://spreadsheets.google.com/feeds/list/1P74T0LWVjk5DmChSfvFUmYBB31o0pGNbqZsBADK_1kY/od6/public/values?alt=json');
-            $json = file_get_contents('https://spreadsheets.google.com/feeds/list/1tQCaj3LUVwH0tBuPrfBY2dOJuF-qzpYEdOqGdNvJRLc/od6/public/values?alt=json');
+            // $json = file_get_contents('https://spreadsheets.google.com/feeds/list/1tQCaj3LUVwH0tBuPrfBY2dOJuF-qzpYEdOqGdNvJRLc/od6/public/values?alt=json');
+            // $data = json_decode($json, true);
+            // $result = array();
+            // $isKeyWord ='0';
 
-            $data = json_decode($json, true);
-            $result = array();
-            $isKeyWord ='0';
+            // foreach ($data['feed']['entry'] as $item) {
+            //     $keywords = explode(',', $item['gsx$keyword']['$t']);
+            //     foreach ($keywords as $keyword) {
+            //         if (mb_strpos($message['text'], $keyword) !== false) {
+            //             $candidate = array(
+            //                 'thumbnailImageUrl' => $item['gsx$photourl']['$t'],
+            //                 'title' => $item['gsx$title']['$t'],
+            //                 'text' => $item['gsx$title']['$t'],
+            //                 'actions' => array(
+            //                     array(
+            //                     'type' => 'uri',
+            //                     'label' => '查看詳情',
+            //                     'uri' => $item['gsx$url']['$t'],
+            //                     ),
+            //                 ),
+            //             );
+            //         array_push($result, $candidate);
+            //          $isKeyWord = '1';
+            //         }else{
+            //          $isKeyWord = '0';
 
-            foreach ($data['feed']['entry'] as $item) {
-                $keywords = explode(',', $item['gsx$keyword']['$t']);
-                foreach ($keywords as $keyword) {
-                    if (mb_strpos($message['text'], $keyword) !== false) {
-                        $candidate = array(
-                            'thumbnailImageUrl' => $item['gsx$photourl']['$t'],
-                            'title' => $item['gsx$title']['$t'],
-                            'text' => $item['gsx$title']['$t'],
-                            'actions' => array(
-                                array(
-                                'type' => 'uri',
-                                'label' => '查看詳情',
-                                'uri' => $item['gsx$url']['$t'],
-                                ),
-                            ),
-                        );
-                    array_push($result, $candidate);
-                     $isKeyWord = '1';
-                    }else{
-                      $isKeyWord = '0';
-
-                    }
-                }
-            }
+            //         }
+            //     }
+            // }
 
             switch ($message['type']) {
                 case 'text':
-                    if($isKeyWord == '1'){
-                        $client->replyMessage(array(
-                            'replyToken' => $event['replyToken'],
-                            'messages' => array(
-                                array(
-                                    'type' => 'text',
-                                    'text' => $message['text'].'讓我想想喔…',
-                                ),
-                                array(
-                                    'type' => 'template',
-                                    'altText' => '為您推薦下列美食：',
-                                    'template' => array(
-                                        'type' => 'carousel',
-                                        'columns' => $result,
-                                    ),
-                                ),
-                                array(
-                                    'type' => 'text',
-                                    'text' => '這些都超好吃，真心不騙！',
-                                ),
-                                array(
-                                    'type' => 'sticker',
-                                    'packageId' => '1',
-                                    'stickerId' => '2',
-                                ),
-                            ),
-                        ));
-                    }else{
+                    // if($isKeyWord == '1'){
+                    //     $client->replyMessage(array(
+                    //         'replyToken' => $event['replyToken'],
+                    //         'messages' => array(
+                    //             array(
+                    //                 'type' => 'text',
+                    //                 'text' => $message['text'].'讓我想想喔…',
+                    //             ),
+                    //             array(
+                    //                 'type' => 'template',
+                    //                 'altText' => '為您推薦下列美食：',
+                    //                 'template' => array(
+                    //                     'type' => 'carousel',
+                    //                     'columns' => $result,
+                    //                 ),
+                    //             ),
+                    //             array(
+                    //                 'type' => 'text',
+                    //                 'text' => '這些都超好吃，真心不騙！',
+                    //             ),
+                    //             array(
+                    //                 'type' => 'sticker',
+                    //                 'packageId' => '1',
+                    //                 'stickerId' => '2',
+                    //             ),
+                    //         ),
+                    //     ));
+                    // }else{
                         $m_message = $message['text'];
                         if($m_message!="" && preg_match('/\肉肉/i',$m_message))
                         {
@@ -153,7 +150,7 @@ foreach ($client->parseEvents() as $event) {
                             )
                             ));
                         }
-                    }
+                  //  }
 
                     break;
                 default:
